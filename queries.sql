ef71a872972fd9087d6e6ab6e9775e99000571c1
Fominochek <168715222+Fominochek@users.noreply.github.com>
2024-05-16 19:34:47 +0300

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -60,4 +60,60 @@ order by extract(DOW from sales.sale_date),
 	функцию TO_CHAR; подсчет выручки через SUM и произведения цены и кол-ва товвара, который был продан, округление через ROUND; группировка через GROUP BY; 
 	сортировка через ORDER BY, чтобы сортировка была не в алфавитном порядке по дню недели, используется функция EXTRACT.
 
+6.
+Отчёт 1.
+with tab1 as(
+	select 
+		case when age between 16 and 25 then '16-25'
+    		 when age between 26 and 40 then '26-40'
+    		 when age>40 then '40+' end as age_category,
+    		 count(age) as count
+	from customers
+	group by age)
+select
+	age_category,
+	sum(count) as count
+from tab1
+group by age_category
+order by age_category;
+//с помощью оператора WITH создаем временную таблицу, с помощью оператора CASE выводим возрастные категории, с помощью функции COUNT подсчитываем количество 
+	человек в каждой возрастной группе, далее в основном запросе считаем суммарное количество человек в каждой группе, группируем и сортируем по возрастной группе.
+
+Отчёт 2.
+select
+	to_char(sales.sale_date,'YYYY-MM') as date,
+	count(distinct(customer_id)) as total_customers,
+	round(sum(quantity)*sum(price),2) as income
+from sales 
+inner join products 
+on sales.product_id=products.product_id 
+group by to_char(sales.sale_date,'YYYY-MM')
+order by to_char(sales.sale_date,'YYYY-MM');
+//преобразование даты в формат год-месяц с помощью функции to_char, подсчет уникальных покупателей с помощью count и distinct, подсчет выручки от покупателей через 
+	суммирование количества умноженное на суммирование цены, выручка округляется через round до двуз цифр  после запятойб группировка и сортировка по дате.
+
+Отчёт 3.
+with prod as(
+	select
+		products.product_id,
+		price*quantity as sal
+	from products
+	inner join sales
+	on products.product_id=sales.product_id)
+select distinct on(customers.first_name, customers.last_name, employees.first_name, employees.last_name)
+	concat(customers.first_name, customers.last_name) as customer,
+	sale_date,
+	concat(employees.first_name, employees.last_name) as seller
+from sales	
+inner join customers
+on customers.customer_id=sales.customer_id 
+inner join employees
+on employee_id = sales_person_id
+inner join prod 
+on prod.product_id=sales.product_id 
+where prod.sal = 0;
+//в подзапросе расчитывается выручка умножением цены на количество по каждому продукту, далее в основном запросе с помощью distinct on выбираются уникальные покупатели
+	и продавцы, с помощью concat объединяются фамилии и имена покупателей и продавцов, с помощью inner join объединяются таблицы, с помощью where идет выборка покупателей, 
+	которые купили акционный товар и продавцы, которые продали акционный товар.
+
 
